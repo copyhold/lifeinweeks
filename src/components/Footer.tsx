014f8dcc4d711b1fca732aa12ac32f986e78c720
copyhold <ilya.novojilov@logz.io>
2025-03-31 19:11:24 +0300

    Add fast life scrolling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,19 +24,46 @@ width: 100%;
 height: 100%;
 --past-color: color(from var(--background-color) xyz calc(x - 0.1) calc(y - 0.1) calc(z - 0.1));
 --future-color: color(from var(--background-color) xyz calc(x + 0.1) calc(y + 0.1) calc(z + 0.1));
+cursor: pointer;
 ${({ percent }) => `
 background: linear-gradient(to right, var(--past-color) ${percent}%, var(--future-color) ${percent}%);
 `};
+&::after {
+content: '';
+height: 100%;
+width: 2px;
+background-color: red;
+position: absolute;
+top: 0;
+animation: marker-position linear both;
+animation-timeline: scroll(root);
+}
+@keyframes marker-position {
+  from {
+    transform: translateX(0cqi);
+  }
+  to {
+    transform: translateX(99cqi);
+  }
+}
 }`;
 export const Footer: React.FC = () => {
   const {birthday, yearsSinceBirth} = useAppStore();
+
+  const scrollToLifePosition = (e: React.ClickEvent) => {
+    const scrollPosition = window.scrollMaxY * e.clientX / e.target.scrollWidth;
+    window.scrollTo({
+      top: scrollPosition,
+      behavior: 'smooth',
+    });
+  }
 
   const percent = useMemo(() => {
     return Math.round(100 * yearsSinceBirth(new Date()) / life_length);
   }, [birthday]);
   return (
     <FooterContainer>
-    <Minimap percent={percent} />
+    <Minimap percent={percent} onClick={scrollToLifePosition} />
     </FooterContainer>
   );
 }
